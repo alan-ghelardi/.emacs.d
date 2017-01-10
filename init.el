cd719d3ce66cea7c27d4f1393798bc557c1398f2
alan-ghelardi <alan.ghelardi@gmail.com>
2017-01-10 23:18:04 +0000

    Remove the better-defaults package since it appears to be conflicting with Emacspeak.

--- init.el
+++ init.el
@@ -38,7 +38,6 @@
 ;; These packages can be installed with M-x package-install.
 ;; Add new packages to the list below:  
 (defvar my-packages '(
-  better-defaults
   projectile
 clojure-mode
   cider))
@@ -46,3 +45,15 @@ clojure-mode
 (dolist (p my-packages)
   (unless (package-installed-p p)
     (package-install p)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (cider clojure-mode projectile))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
