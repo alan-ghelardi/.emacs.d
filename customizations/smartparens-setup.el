38f42cd66be5d9a4254796bc820dbfc3e967d12b
alan-ghelardi <alan.ghelardi@gmail.com>
2017-01-24 00:32:29 +0000

    Add more keybindings for moving between sexps.

--- customizations/smartparens-setup.el
+++ customizations/smartparens-setup.el
@@ -6,7 +6,11 @@
 
   :bind (
   ("C-M-a" . sp-beginning-of-sexp)   
-  ("C-M-E" . sp-end-of-sexp))
+  ("C-M-E" . sp-end-of-sexp)
+  ("C-M-f" . sp-forward-sexp)
+  ("C-M-b" . sp-backward-sexp)
+  ("C-M-n" . sp-next-sexp)
+  ("C-M-p" . sp-previous-sexp))
   
   :config
   (progn
