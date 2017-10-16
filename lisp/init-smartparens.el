10e085d9179f2619e6be78eb79cb206eda948212
Alan Ghelardi <alan.ghelardi@nubank.com.br>
2017-10-16 17:14:53 -0200

    Add a key binding to slurp sexps.

--- lisp/init-smartparens.el
+++ lisp/init-smartparens.el
@@ -10,13 +10,14 @@
   :bind (
          ("C-k" . sp-kill-sexp)
          ("C-M-k" . sp-kill-hybrid-sexp)
-         ("C-M-a" . sp-beginning-of-sexp)   
+         ("C-M-a" . sp-beginning-of-sexp)
          ("C-M-E" . sp-end-of-sexp)
          ("C-M-f" . sp-forward-sexp)
          ("C-M-b" . sp-backward-sexp)
          ("C-M-n" . sp-next-sexp)
-         ("C-M-p" . sp-previous-sexp))
-  
+         ("C-M-p" . sp-previous-sexp)
+         ("C-M-;" . sp-add-to-previous-sexp))
+
   :config
   (progn
     (show-smartparens-global-mode t)))
