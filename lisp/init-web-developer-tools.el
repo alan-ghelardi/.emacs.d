ef885988f95fa1797a711c118cdc1b90b6463db6
Alan Ghelardi <alan.ghelardi@nubank.com.br>
2017-11-28 11:52:04 -0200

    Change the default indentation for JSON files.

--- lisp/init-web-developer-tools.el
+++ lisp/init-web-developer-tools.el
@@ -1,4 +1,3 @@
-
 (defun my-web-mode-hook ()
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
@@ -9,16 +8,21 @@
 
   :config
   (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode)) 
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.xml\\'" . web-mode))
   (add-hook 'web-mode-hook 'my-web-mode-hook))
+
+(defun my-json-mode-hook ()
+  (make-local-variable 'js-indent-level)
+  (setq js-indent-level 2))
 
 (use-package json-mode
   :ensure t
   :config
   (add-to-list 'auto-mode-alist '("\\.json\\'" . json-mode))
-  (add-to-list 'auto-mode-alist '("\\.json\\.base\\'" . json-mode)))
+  (add-to-list 'auto-mode-alist '("\\.json\\.base\\'" . json-mode))
+  (add-hook 'json-mode-hook 'my-json-mode-hook))
 
 (use-package restclient
   :ensure t
