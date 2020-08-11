87456531f19f395badf821f5ea7b6faae5a547d0
Alan Ghelardi <alan.ghelardi@nubank.com.br>
2020-08-11 11:21:04 -0300

    Deactivate emacspeak-speak-messages on go-mode.

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -1,7 +1,12 @@
+(defun golang-disable-emacspeak-message-echo ()
+  "Disable emacspeak message echo on go-mode."
+  (setq-local emacspeak-speak-messages nil))
+
 (use-package lsp-mode
   :ensure t
   :commands (lsp lsp-deferred)
-  :hook (go-mode . lsp-deferred))
+  :hook (go-mode . lsp-deferred)
+  (go-mode . golang-disable-emacspeak-message-echo))
 
 ;; Set up before-save hooks to format buffer and add/delete imports.
 (defun lsp-go-install-save-hooks ()
