56b34c5ef2ede215d26fa04892fdf011ad2ebe6e
Alan Ghelardi <alan.ghelardi@nubank.com.br>
2021-08-10 10:13:59 -0300

    Configure Emacspeak to not read caps.

--- lisp/init-emacspeak.el
+++ lisp/init-emacspeak.el
@@ -10,26 +10,33 @@
 ;; I prefer to start the synthesizer in my mother language and switch to english when necessary.
 (defconst my-emacspeak-speech-language "pt-br")
 
+;; Synthesizer's rate
 (defconst my-emacspeak-speech-rate 400)
 
+;; Sound's theme used by Emacspeak
 (defconst my-emacspeak-sound-theme "3d/")
 
 (add-to-list 'load-path my-emacspeak-src-dir)
 
 (add-hook 'prog-mode-hook (lambda ()
-                            (setq emacspeak-audio-indentation nil)))
+                            (setq emacspeak-audio-indentation nil)
+                            (when dtk-caps
+      (setq dtk-caps nil)
+      (setq-default dtk-caps nil)
+      (when dtk-split-caps
+        (setq dtk-split-caps nil)
+        (setq-default dtk-split-caps nil)))))
 
 ;; Start Emacspeak.
 (load-file (concat my-emacspeak-src-dir "/lisp/emacspeak-setup.el"))
 
 (with-eval-after-load "emacspeak"
   (setq dtk-program my-emacspeak-speech-server)
-  (setq dtk-caps nil)
+  (setq-default dtk-caps nil)
+  (setq-default dtk-split-caps nil)
   (dtk-set-language my-emacspeak-speech-language)
             (dtk-set-rate my-emacspeak-speech-rate 1)
             (emacspeak-sounds-select-theme my-emacspeak-sound-theme))
-
-                                        ;(require 'emacspeak-muggles)
 
 ;; Inline speech support for some packages.
 
